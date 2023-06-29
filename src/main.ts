928d32329d7518d218937a630dfd9bfb96c67cac
pathu-221 <prathamaggarwal23@gmail.com>
2023-06-29 18:24:07 +0530

    added cors

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,9 @@ import { join } from 'path';
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   
+  app.enableCors({
+    origin: ['http://localhost:5173']
+  })
   app.useStaticAssets(join(__dirname, '..', 'public'));
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
   app.setViewEngine('hbs');
