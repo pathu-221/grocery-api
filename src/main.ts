971d74c5c27ef987cbbf4d45aa1bec81d62d6b8a
pathu-221 <prathamaggarwal23@gmail.com>
2024-04-12 13:53:07 +0530

    configure cors

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,7 @@ async function bootstrap() {
     origin: [
       'http://localhost:5173',
       'http://localhost:5174',
-      'grocery.prathamaggarwal.me',
+      'https://grocery.prathamaggarwal.me',
     ],
   });
   app.useStaticAssets(join(__dirname, '../../', 'public'));
